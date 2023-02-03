2aa3917b7e83b118e37d6314b39b406372e2c906
a-wotton <a_wotton@fanshaweonline.ca>
2023-02-03 09:49:14 -0500

    background images are done

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ puzzleBoard = document.querySelector(".puzzle-board");
 //function go in the middle - this is a 1 to many or 1 to 1 connection to elements in the DOM
 
 function changeBGImage() {
-	debugger;
-	puzzleBoard.style.backGroundImage = "url(../backGroundImage)"
+	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
 } 
 
 //event handling at the bottom - how is the user going to interact with the elements / controls you provide?
